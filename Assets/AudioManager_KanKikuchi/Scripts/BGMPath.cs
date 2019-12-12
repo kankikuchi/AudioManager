b1d8ffac439d21730c0f788908e0a4a1badd469d
kan.kikuchi <kan.kikuchi.000@gmail.com>
2019-12-12 12:54:05 +0900

    オーディオファイルと設定の更新

--- Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -8,7 +8,6 @@ public static class BGMPath{
 	public const string BATTLE27    = "BGM/Battle/battle27";
 	public const string FANTASY14   = "BGM/fantasy14";
 	public const string HEARTBEAT01 = "BGM/heartbeat01";
-	public const string TEST        = "SE/Test";
 
 }
 
